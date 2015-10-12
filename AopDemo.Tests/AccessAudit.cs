2ce1e0292935af97219313a202612046559464d3
James Stumme <jstumme@pillartechnology.com>
2015-10-12 15:35:14 -0500

    Smocks bug #12

---
+++ AopDemo.Tests/AccessAudit.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using Smocks;
+using System;
+
+namespace AopDemo.Tests
+{
+	[TestFixture]
+	public class AccessAudit
+	{
+		[Test]
+		public void ReadingNameUpdatesTimestamp()
+		{
+
+			Smock.Run (ctx => {
+				var now = DateTime.Now.AddMonths (18);
+				var person = new Person { FirstName = "John", LastName = "Doe", Age = 24 };
+
+				ctx.Setup (() => DateTime.Now).Returns (now);
+
+				var name = person.FirstName;
+
+				Assert.AreEqual (now, person.AccessedOn);
+			});
+		}
+	}
+}
+
---
+++ AopDemo/Interfaces/IAudit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AopDemo.Interfaces
+{
+	public interface IAudit
+	{
+		DateTime CreatedOn { get; set; }
+		DateTime AccessedOn { get; set; }
+		DateTime ModifiedOn { get; set; }
+	}
+}
+
