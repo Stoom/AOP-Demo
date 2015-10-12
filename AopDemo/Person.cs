a196601327f7e44c38f122427b03e2a37f9b42ee
James Stumme <jstumme@pillartechnology.com>
2015-10-12 15:35:14 -0500

    Smocks bug #12

---
+++ AopDemo/Person.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using AopDemo.Interfaces;
+
+namespace AopDemo
+{
+	public class Person : IAudit
+	{
+		#region IAudit implementation
+		public DateTime CreatedOn { get; set; }
+		public DateTime AccessedOn { get; set; }
+		public DateTime ModifiedOn { get; set; }
+		#endregion
+
+		public String FirstName { get; set; }
+		public String LastName { get; set; }
+		public int Age { get; set; }
+	}
+}
+
