383730b139fa92d210dbf50c01b712f21a633543
James Stumme <jstumme@pillartechnology.com>
2015-10-13 15:19:14 -0500

    Passed all modify audit tests.

--- AopDemo.Tests/ModifyAuditTests.cs
+++ AopDemo.Tests/ModifyAuditTests.cs
@@ -1,10 +1,45 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
+
 namespace AopDemo.Tests
 {
+	[TestFixture]
 	public class ModifyAuditTests
 	{
-		public ModifyAuditTests()
+		[Test]
+		public void ModifyingFirstNameUpdatesTimestamp()
 		{
+			var person = new Person();
+			var before = DateTime.Now;
+			person.FirstName = "John";
+			var after = DateTime.Now;
+
+			Assert.Greater(person.ModifiedOn, before);
+			Assert.Less(person.ModifiedOn, after);
+		}
+
+		[Test]
+		public void ModifyingLastNameUpdatesTimestamp()
+		{
+			var person = new Person();
+			var before = DateTime.Now;
+			person.LastName = "Doe";
+			var after = DateTime.Now;
+
+			Assert.Greater(person.ModifiedOn, before);
+			Assert.Less(person.ModifiedOn, after);
+		}
+
+		[Test]
+		public void ModifyingAgeUpdatesTimestamp()
+		{
+			var person = new Person();
+			var before = DateTime.Now;
+			person.Age = 42;
+			var after = DateTime.Now;
+
+			Assert.Greater(person.ModifiedOn, before);
+			Assert.Less(person.ModifiedOn, after);
 		}
 	}
 }
