2e7e1fb7bc4c9bcb5de621a4dda07f84217312eb
James Stumme <jstumme@pillartechnology.com>
2015-10-13 14:52:11 -0500

    Passed all access audit tests.

--- AopDemo/Person.cs
+++ AopDemo/Person.cs
@@ -11,9 +11,46 @@ namespace AopDemo
 		public DateTime ModifiedOn { get; set; }
 		#endregion
 
-		public String FirstName { get; set; }
-		public String LastName { get; set; }
-		public int Age { get; set; }
+		public String FirstName
+		{ 
+			get
+			{
+				AccessedOn = DateTime.Now;
+				return _firstName;
+			}
+			set
+			{
+				_firstName = value;
+			}
+		}
+		public String LastName
+		{ 
+			get
+			{
+				AccessedOn = DateTime.Now;
+				return _lastName;
+			}
+			set
+			{
+				_lastName = value;
+			}
+		}
+		public int Age
+		{ 
+			get
+			{
+				AccessedOn = DateTime.Now;
+				return _age;
+			}
+			set
+			{
+				_age = value;
+			}
+		}
+
+		private String _firstName;
+		private String _lastName;
+		private int _age;
 	}
 }
 
