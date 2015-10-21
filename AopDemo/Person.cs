9b4ce8bd62249ffa8daa9d50198b060e450d602e
James Stumme <jstumme@pillartechnology.com>
2015-10-21 11:24:00 -0500

    Fixed tests for AOP.

--- AopDemo/Person.cs
+++ AopDemo/Person.cs
@@ -4,16 +4,19 @@ using AopDemo.Aop.Attributes;
 
 namespace AopDemo
 {
-	public class Person : IAudit
-	{
+    public class Person : IAudit
+    {
         #region IAudit implementation
         public DateTime CreatedOn { get; set; } = DateTime.Now;
         public DateTime AccessedOn { get; set; } = DateTime.Now;
         public DateTime ModifiedOn { get; set; } = DateTime.Now;
-		#endregion
+        #endregion
 
+        [Audit]
         public String FirstName { get; set; }
+        [Audit]
         public String LastName { get; set; }
+        [Audit]
         public int Age { get; set; }
 	}
 }
